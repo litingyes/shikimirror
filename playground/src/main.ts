278a2a2d196204182b695167181fc12798978e40
litingyes <luz.liting@gmail.com>
2025-03-26 21:46:11 +0800

    test: fork shiki playground

---
+++ playground/src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
+import App from './App.vue'
+
+import '@unocss/reset/tailwind.css'
+import 'uno.css'
+
+const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+app.use(router)
+app.mount('#app')
